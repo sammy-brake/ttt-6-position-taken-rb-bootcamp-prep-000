daee0db590f420f89073893875ef3468f0cf027f
Samantha brake <sammy.brake@gmail.com>
2018-07-24 18:29:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,10 @@ def position_taken?(board, index)
  
     
   if board[index] == " " || "" || nil 
-     return  false
+     false
       
    else board[index] == "X" || "O"
-     return true
+     true
   
 end
 end
